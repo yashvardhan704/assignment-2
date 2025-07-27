26d080d9010855a472e307b6c15c8179b44fa747
Yashvardhan Agrawal <yashagrawal70405@gmail.com>
2025-07-27 20:07:12 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,5 @@
+number=int(input("please enter your number: "))
+if number%2==0:
+    print("the follwing number is even")
+else:
+    print("the following number is odd")
---
+++ task2.py
@@ -0,0 +1,4 @@
+sum=0
+for num in range(1,51):
+    sum= sum+num
+print(f"the sum of number from 1 to 50 is: {sum}")
